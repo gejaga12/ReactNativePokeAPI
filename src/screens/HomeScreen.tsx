a0bceb39c041aa4b4e148440dbeb07caeccf4001
gejaga12 <gejaga12@hotmail.com>
2023-02-06 16:20:40 -0300

    pokemonCard creada

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { ActivityIndicator, Image, Text } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PokemonCard } from '../components/PokemonCard'
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
+import { styles } from '../theme/appTheme'
+
+export const HomeScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
+
+  return (
+    <>
+
+      <Image source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG} />
+
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={(pokemon => pokemon.id)}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        // HEADER DE LA TABLA
+        ListHeaderComponent={(
+          <Text style={{
+            ...styles.title,
+            ...styles.globalMargin,
+            marginBottom: top + 20,
+            top: top + 20
+          }}>Pokedex</Text>
+        )}
+        renderItem={({ item }) => (
+          <PokemonCard pokemon={item} />
+        )}
+        // INFINITE SCROLL
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        // INDICADOR DE ACTIVIDAD
+        ListFooterComponent={<ActivityIndicator style={{ height: 100 }} size={20} color="red" />}
+
+      />
+
+
+
+    </>
+  )
+}
+
+
